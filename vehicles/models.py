2134d5db791a0e4d5350fc5762c09d22c3ca140e
mszahan <mszahan1@gmail.com>
2024-10-24 23:10:24 +0600

    created 3 models in vehicles

--- vehicles/models.py
+++ vehicles/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+
+YESNO_CHOICES = (
+    (True, 'Yes'),
+    (False, 'No')
+)
+
+
+class VehicleModel(models.Model):
+    name = models.CharField(verbose_name='Model', 
+                            max_length=75,
+                            unique=True,
+                            blank=True,
+                            null=True,)
+    
+
+class Engine(models.Model):
+    name = models.CharField(verbose_name='Engine', 
+                            max_length=75,
+                            blank=True,
+                            null=True)
+
+
+class Vehicle(models.Model):
+    vin = models.CharField(verbose_name='VIN', 
+                           max_length=17,
+                           unique=True,
+                           blank=True,
+                           null=True)
+    sold = models.BooleanField(verbose_name='Sold?',
+                               choices=YESNO_CHOICES,
+                               default=False,
+                               blank=True,
+                               null=True)
